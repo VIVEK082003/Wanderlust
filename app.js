08e9b439b382f140b1f6c724249da07b78c80871
VIVEK082003 <vivekawtani082003@gmail.com>
2025-01-23 15:13:24 +0530

    Add

--- app.js
+++ app.js
@@ -81,15 +81,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// app.get("/demouser", async (req, res, next) => {
-//   let fakeUser = new User({
-//     email: "student@gmail.com",
-//     username: "Student",
-//   });
 
-//   let registeredUser = await User.register(fakeUser, "helloworld");
-//   res.send(registeredUser);
-// });
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root");
